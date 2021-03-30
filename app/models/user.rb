5702d2228d8ee5993127152d2376f397ef5e1de3
enkog <nkirukaogu@gmail.com>
2021-03-30 02:16:32 +0100

    Add friendship features in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,24 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :friendships, foreign_key: 'user_id'
-  has_many :friends, through: :friendships, class_name: 'User'
+  has_many :friends, through: :friendships, foreign_key: 'friend_id'
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend unless friendship.confirmed}.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user unless friendship.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
